0b883d904b2f90da527e2267dcb840ab50a3b8ee
Sherry List <shahrzad.aziminia@gmail.com>
2018-11-24 14:01:13 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'joke';
+}
